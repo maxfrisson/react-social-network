fc33dc505e1cae0f5696073af930e1a1617142b6
Max Frisson <max.chebonenko@gmail.com>
2021-05-01 02:31:40 +0300

    master

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,44 +1,11 @@
+import axios from "axios";
 import styles from "./users.module.css";
 
 let Users = (props) => {
-  if (props.users.length === 0) {
-    props.setUsers (
-      [
-        {
-          id: 1,
-          photoUrl: "https://htmlstream.com/preview/unify-v2.6.3/assets/img-temp/400x450/img5.jpg",
-          fullName: "Max",
-          status: "Hello World!!!",
-          location: { city: "Kiev", country: "Ukraine" },
-          followed: true,
-        },
-        {
-          id: 2,
-          photoUrl: "https://i1.rgstatic.net/ii/profile.image/820485688414213-1572630440525_Q512/Melissa-Cox-2.jpg",
-          fullName: "Vika",
-          status: "Hi World!!!",
-          location: { city: "Brest", country: "Belarus" },
-          followed: true,
-        },
-        {
-          id: 3,
-          photoUrl: "https://newdoorfiji.com/wp-content/uploads/2018/03/profile-img-1.jpg",
-          fullName: "Lera",
-          status: "Bye World!!!",
-          location: { city: "Tula", country: "Russia" },
-          followed: false,
-        },
-        {
-          id: 4,
-          photoUrl: "https://preview.keenthemes.com/metronic-v4/theme/assets/pages/media/profile/profile_user.jpg",
-          fullName: "Michael",
-          status: "How are you World???",
-          location: { city: "Kirov", country: "Russia" },
-          followed: false,
-        },
-      ]
-    )
-  }
+
+  axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response=>{
+    props.setUsers(response.data.items);
+  })
 
   return (
     <div>
@@ -70,12 +37,12 @@ let Users = (props) => {
           </span>
           <span>
             <span>
-              <div>{u.fullName}</div>
+              <div>{u.name}</div>
               <div>{u.status}</div>
             </span>
             <span>
-              <div>{u.location.country}</div>
-              <div>{u.location.city}</div>
+              <div>{"u.location.country"}</div>
+              <div>{"u.location.city"}</div>
             </span>
           </span>
         </div>
