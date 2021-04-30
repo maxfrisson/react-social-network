bb7c3952111a4a8efc7d79a8435a930c54e8ce48
Max Frisson <max.chebonenko@gmail.com>
2021-04-30 17:23:03 +0300

    master

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,6 +1,45 @@
 import styles from "./users.module.css";
 
 let Users = (props) => {
+  if (props.users.length === 0) {
+    props.setUsers (
+      [
+        {
+          id: 1,
+          photoUrl: "https://htmlstream.com/preview/unify-v2.6.3/assets/img-temp/400x450/img5.jpg",
+          fullName: "Max",
+          status: "Hello World!!!",
+          location: { city: "Kiev", country: "Ukraine" },
+          followed: true,
+        },
+        {
+          id: 2,
+          photoUrl: "https://i1.rgstatic.net/ii/profile.image/820485688414213-1572630440525_Q512/Melissa-Cox-2.jpg",
+          fullName: "Vika",
+          status: "Hi World!!!",
+          location: { city: "Brest", country: "Belarus" },
+          followed: true,
+        },
+        {
+          id: 3,
+          photoUrl: "https://newdoorfiji.com/wp-content/uploads/2018/03/profile-img-1.jpg",
+          fullName: "Lera",
+          status: "Bye World!!!",
+          location: { city: "Tula", country: "Russia" },
+          followed: false,
+        },
+        {
+          id: 4,
+          photoUrl: "https://preview.keenthemes.com/metronic-v4/theme/assets/pages/media/profile/profile_user.jpg",
+          fullName: "Michael",
+          status: "How are you World???",
+          location: { city: "Kirov", country: "Russia" },
+          followed: false,
+        },
+      ]
+    )
+  }
+
   return (
     <div>
       {props.users.map((u) => (
