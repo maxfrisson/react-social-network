e51d3e6c6271dac604999ef7fb81b88e83ab18c1
Max Frisson <max.chebonenko@gmail.com>
2021-05-08 11:07:40 +0300

    master

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Route } from "react-router";
 import "./App.css";
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
-import Header from "./components/Header/Header";
+import HeaderContainer from "./components/Header/HeaderContainer";
 import Music from "./components/Music/Music";
 import Navbar from "./components/Navbar/Navbar";
 import News from "./components/News/News";
@@ -12,7 +12,7 @@ import UsersContainer from "./components/Users/UsersContainer";
 const App = (props) => {
   return (
     <div className="app-wrapper">
-      <Header />
+      <HeaderContainer />
       <Navbar />
       <div className="app-wrapper-content">
         <Route path="/profile/:userId?" render={() => <ProfileContainer />} />
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,19 @@
-import style from './Header.module.css';
+import { NavLink } from "react-router-dom";
+import style from "./Header.module.css";
 
-const Header = () => {
+const Header = (props) => {
   return (
     <header className={style.header}>
-        <img
-          className={style.image} src="https://cdn.logo.com/hotlink-ok/logo-social-sq.png"
-          alt=""
-        />
-      </header>
-  )
-}
+      <img
+        className={style.image}
+        src="https://cdn.logo.com/hotlink-ok/logo-social-sq.png"
+        alt=""
+      />
+      <div className={style.loginBlock}>
+        {props.isAuth ? props.login : <NavLink to="/login">Login</NavLink>}
+      </div>
+    </header>
+  );
+};
 
 export default Header;
