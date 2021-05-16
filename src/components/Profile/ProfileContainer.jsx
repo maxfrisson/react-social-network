d043dd5582b55bd2f3378697674f406191b2c619
Max Frisson <max.chebonenko@gmail.com>
2021-05-16 23:22:04 +0300

    master

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -33,14 +33,12 @@ class ProfileContainer extends React.Component {
   }
 }
 
-let mapStateToProps = (state) => {
-  return {
-    profile: state.profilePage.profile,
-    status: state.profilePage.status,
-    authorizedUserId: state.auth.id,
-    isAuth: state.auth.isAuth,
-  };
-};
+let mapStateToProps = (state) => ({
+  profile: state.profilePage.profile,
+  status: state.profilePage.status,
+  authorizedUserId: state.auth.id,
+  isAuth: state.auth.isAuth,
+});
 
 export default compose(
   connect(mapStateToProps, { getUserProfile, getStatus, updateStatus }),
