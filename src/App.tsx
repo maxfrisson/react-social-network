52c77e52e856853bff5780e4ff17f67d37841ad7
Max Frisson <max.chebonenko@gmail.com>
2021-06-19 22:55:15 +0300

    change layout and nav

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ import Settings from "./components/Settings/Settings";
 import { UsersPage } from "./components/Users/UsersContainer";
 import { initializeApp } from "./redux/appReducer";
 import store, { AppStateType } from "./redux/redux-store";
-import { BrowserRouter, Link } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { withSuspense } from "./hoc/withSuspense";
 
@@ -48,53 +48,8 @@ class App extends React.Component<MapPropsType & DispatchPropsType> {
     return (
       <Layout>
         <Header />
-        <Content style={{ padding: "0 50px" }}>
-          <Breadcrumb style={{ margin: "16px 0" }}>
-            <Breadcrumb.Item>Home</Breadcrumb.Item>
-            <Breadcrumb.Item>List</Breadcrumb.Item>
-            <Breadcrumb.Item>App</Breadcrumb.Item>
-          </Breadcrumb>
+        <Content style={{ padding: "0 50px", maxWidth: "1200px", margin: "0 auto" }}>
           <Layout className="site-layout-background" style={{ padding: "24px 0" }}>
-            <Sider className="site-layout-background" width={200}>
-              <Menu
-                mode="inline"
-                defaultSelectedKeys={["1"]}
-                defaultOpenKeys={["sub1"]}
-                style={{ height: "100%" }}
-              >
-                <SubMenu key="sub1" icon={<UserOutlined />} title="My Profile">
-                  <Menu.Item key="1">
-                    <Link to="/profile">Profile</Link>
-                  </Menu.Item>
-                  <Menu.Item key="2">
-                    <Link to="/dialogs">Messages</Link>
-                  </Menu.Item>
-                </SubMenu>
-                <SubMenu key="sub2" icon={<LaptopOutlined />} title="Developers">
-                  <Menu.Item key="3">
-                    <Link to="/users">Users</Link>
-                  </Menu.Item>
-                </SubMenu>
-                <SubMenu key="sub3" icon={<NotificationOutlined />} title="Chat">
-                  <Menu.Item key="4">
-                    <Link to="/chat">Chat</Link>
-                  </Menu.Item>
-                </SubMenu>
-                <SubMenu key="sub4" icon={<NotificationOutlined />} title="Music">
-                  <Menu.Item key="5">
-                    <Link to="/settings">Your music</Link>
-                  </Menu.Item>
-                  <Menu.Item key="6">
-                    <Link to="/settings">Top tracks</Link>
-                  </Menu.Item>
-                </SubMenu>
-                <SubMenu key="sub5" icon={<NotificationOutlined />} title="Settings">
-                  <Menu.Item key="7">
-                    <Link to="/settings">Settings</Link>
-                  </Menu.Item>
-                </SubMenu>
-              </Menu>
-            </Sider>
             <Content style={{ padding: "0 24px", minHeight: 280 }}>
               <Switch>
                 <Route
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,43 +1,61 @@
+import React from "react";
 import { Link } from "react-router-dom";
-import { UserOutlined } from "@ant-design/icons";
+import {
+  UserOutlined,
+  UsergroupAddOutlined,
+  WechatOutlined,
+  MessageOutlined,
+} from "@ant-design/icons";
 import { Button, Col, Layout, Menu, Row } from "antd";
 import Avatar from "antd/lib/avatar/avatar";
 import { useDispatch, useSelector } from "react-redux";
 import { selectCurrentUserLogin, selectIsAuth } from "../../redux/authSelectors";
 import { logout } from "../../redux/authReducer";
-import React from "react";
+import logo from "../../assets/images/logo.png";
+import style from "./Header.module.css";
 
-export type MapPropsType = {}
+export type MapPropsType = {};
 
 export const Header: React.FC<MapPropsType> = (props) => {
   const isAuth = useSelector(selectIsAuth);
   const login = useSelector(selectCurrentUserLogin);
-  
+
   const dispatch = useDispatch();
   const logoutCallback = () => {
     dispatch(logout());
   };
 
-  const {Header} = Layout;
+  const { Header } = Layout;
 
   return (
     <Header className="header">
-      <div className="logo" />
       <Row>
-        <Col span={20}>
+        <Col span={2}>
+          <Link to="/profile">
+            <img className={style.logo} src={logo} alt="" />
+          </Link>
+        </Col>
+        <Col span={18}>
           <Menu theme="dark" mode="horizontal" defaultSelectedKeys={[""]}>
-            <Menu.Item key="1">
+            <Menu.Item key="1" icon={<UserOutlined />}>
               <Link to="/profile">Profile</Link>
             </Menu.Item>
-            <Menu.Item key="2">
+            <Menu.Item key="2" icon={<MessageOutlined />}>
+              <Link to="/dialogs">Message</Link>
+            </Menu.Item>
+            <Menu.Item key="3" icon={<UsergroupAddOutlined />}>
               <Link to="/users">Users</Link>
+            </Menu.Item>
+            <Menu.Item key="4" icon={<WechatOutlined />}>
+              <Link to="/chat">Chat</Link>
             </Menu.Item>
           </Menu>
         </Col>
         <Col span={4}>
           {isAuth ? (
-            <div style={{color: "#f0f2f5", fontSize: "1.5rem"}}>
-              <Avatar icon={<UserOutlined />} /> {login} - <Button onClick={logoutCallback}>Log out</Button>
+            <div style={{ color: "#f0f2f5", fontSize: "1.5rem" }}>
+              <Avatar icon={<UserOutlined />} /> {login} -{" "}
+              <Button onClick={logoutCallback}>Log out</Button>
             </div>
           ) : (
             <Link to="/login">Login</Link>
