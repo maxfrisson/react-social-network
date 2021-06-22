c58942fdc63d833350a341db3cc98f085028cfb8
Max Frisson <max.chebonenko@gmail.com>
2021-06-22 23:33:46 +0300

    fix navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -14,13 +14,7 @@ const Navbar: React.FC = () => {
           <NavLink to="/users" activeClassName={style.activeLink}>Users</NavLink>
         </div>
         <div className={style.item}>
-          <NavLink to="/news" activeClassName={style.activeLink}>News</NavLink>
-        </div>
-        <div className={style.item}>
           <NavLink to="/music" activeClassName={style.activeLink}>Music</NavLink>
-        </div>
-        <div className={style.item}>
-          <NavLink to="/settings" activeClassName={style.activeLink}>Settings</NavLink>
         </div>
       </nav>
   )
